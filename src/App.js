33bba2d6d5f8e018a7590eca2fd16bf34aae7859
Federico <133919977+FedericoCWW@users.noreply.github.com>
2025-10-26 15:28:06 -0300

    V2

--- src/App.js
+++ src/App.js
@@ -1,14 +1,38 @@
-import React from "react";
+import React, {useState} from "react";
+import axios from 'axios';
 import Search from './componentes/Search.js'
+// https://api-ninjas.com/api/cars
 
 function App() {
+  const apiurl = './services/config.js'; 
+  const search = (e) => {
+    if (e.key === 'Enter') {
+      axios(apiurl + "&s=")
+    }
+  }
+
+  const [state, setState] = useState({
+    s: "",
+    resolve: [],
+    selected: {}
+  });
+
+  const handleInput = (e) => {
+    let s = e.target.value;
+
+    setState(prevState => {
+      return {...prevState, s:s}
+    });
+    console.log(state.s);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <h1>Holaaaa!!</h1>
       </header>
       <main> {/* Aca van los componentes (barra de busqueda, resultados, popups con detalles, etc)*/}
-        <Search />
+        <Search handleInput={handleInput}/>
       </main>
     </div>
   );
--- src/componentes/Search.js
+++ src/componentes/Search.js
@@ -1,10 +1,14 @@
 /* Para los snippets usar la extension: ES7+ React/Redux/React-Native snippets*/
 import React from 'react'
 
-function Search() {
+function Search({handleInput}) {
   return (
     <div className='searchbox-wrap'>
-        <input type='text' placeholder='Buscar un auto...' className='searchbox'/>
+        <input type='text' 
+        placeholder='Buscar un auto...' 
+        className='searchbox' 
+        onChange={handleInput}
+        />
     </div>
   )
 }
